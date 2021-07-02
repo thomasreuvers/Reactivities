d32332550be1238a5c7984e2c691d61571b6ef41
Thomas <thomasreuvers@upcmail.nl>
2021-07-02 10:34:59 +0200

    End of section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,17 +3,18 @@ import React, { useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
+import ActivityFilters from './ActivityFilters';
 import ActivityList from './ActivityList';
 
 export default observer(function ActivityDashboard() {
     const {activityStore} = useStore();
-    const {loadActivities, activityRegistry} = activityStore;
+    const {loadActivities, activityRegistry, loadingInitial} = activityStore;
 
   useEffect(() => {
     if(activityRegistry.size <= 1) loadActivities();
   }, [activityRegistry.size, loadActivities]);
 
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+  if (loadingInitial) return <LoadingComponent content='Loading app' />
 
     return(
         <Grid>
@@ -21,7 +22,7 @@ export default observer(function ActivityDashboard() {
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
-                <h2>Activity filters</h2>
+                <ActivityFilters />
             </Grid.Column>
         </Grid>
     )
